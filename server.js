841e526fb5ff74567f44161ccb2d68df5d45b714
laurence <laurencehall1@outlook.com>
2018-12-13 18:01:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/user', userRoutes)
 app.use('/exercise', exerciseRoutes)
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, (err) => {
 if(err) console.log(err.message);
